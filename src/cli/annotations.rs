a216f7a2263d79783bc1f06661c04a2d29a6fd22
Jérome Eertmans <jeertmans@icloud.com>
2024-01-16 11:50:37 +0100

    chore(log): add logging utilities

--- src/cli/annotations.rs
+++ src/cli/annotations.rs
@@ -5,7 +5,7 @@ use std::{
 
 use anyhow::{Context, Result};
 use clap::{ArgAction, Args, Parser, Subcommand};
-use log::{error, info, warn};
+use log::{debug, error, info, log_enabled, trace, warn, Level::Info};
 use lopdf::{Document, Object, ObjectId};
 use owo_colors::OwoColorize;
 use tabled::{
@@ -32,7 +32,7 @@ impl Execute for Stats {
         W: WriteColor,
     {
         let document = Document::load(&self.file)
-            .with_context(|| format!("Failed to read PDF from: {}", self.file.to_str().unwrap()))?;
+            .with_context(|| format!("Failed to read PDF from: {:?}", self.file))?;
         let mut counters = vec![];
         let mut subtypes = HashSet::new();
 
@@ -96,6 +96,7 @@ impl Execute for Stats {
             let mut record = Vec::with_capacity(subtypes.len());
             let mut total = HashMap::with_capacity(subtypes.len());
 
+            debug!("Summing counts from each page...");
             for counter in counters {
                 for (subtype, count) in counter {
                     *total.entry(subtype).or_insert(0) += count;
@@ -118,6 +119,7 @@ impl Execute for Stats {
             .with(Style::modern());
 
         if stdout.supports_color() {
+            trace!("Stdout supports color so table will be colored");
             table.with(BorderColor::filled(Color::FG_GREEN));
         }
 
@@ -149,18 +151,21 @@ struct Merge {
 fn get_page_annotations_mut(document: &mut Document, page_id: ObjectId) -> &mut Vec<Object> {
     match document.get_dictionary(page_id).unwrap().get(b"Annots") {
         Ok(Object::Reference(ref id)) => {
+            trace!("This page contains a reference to a vector of annotations");
             document
                 .get_object_mut(*id)
                 .and_then(Object::as_array_mut)
                 .unwrap()
         },
         Ok(Object::Array(_)) => {
+            trace!("This page contains a vector of annotations");
             let page = document.get_dictionary_mut(page_id).unwrap();
             page.get_mut(b"Annots")
                 .and_then(Object::as_array_mut)
                 .unwrap()
         },
         Err(_) => {
+            trace!("This page does not contain any annotations, inserting an empty array");
             let page_map = document
                 .get_dictionary_mut(page_id)
                 .unwrap()
@@ -181,8 +186,20 @@ impl Execute for Merge {
     where
         W: WriteColor,
     {
-        info!("Reading reference document from {:?}", self.files[0]);
+        if log_enabled!(Info) {
+            let mut msg = String::from("Processing documents: ");
 
+            self.files
+                .iter()
+                .enumerate()
+                .for_each(|(document_number, file)| {
+                    msg.push_str(&format!("{file:?} (#{document_number})"));
+                    if document_number < self.files.len() - 1 {
+                        msg.push_str(", ");
+                    }
+                });
+            info!("{msg}");
+        }
         let mut main = Document::load(&self.files[0]).with_context(|| {
             format!(
                 "Failed to read PDF from: {}",
@@ -191,18 +208,16 @@ impl Execute for Merge {
         })?;
 
         let pages = main.get_pages();
-        info!("Reference document contains {} pages", pages.len());
+        debug!("Reference document contains {} pages", pages.len());
 
         // Maps page number (note object id) to annotations
         let mut annotations_map = HashMap::new();
 
-        info!("Reading other file(s)...");
-        for file in &self.files[1..] {
-            info!("Reading PDF document from {:?}", file);
+        for (document_number, file) in (1..).zip(&self.files[1..]) {
+            debug!("Processing document #{document_number}");
             let document = Document::load(file)
                 .with_context(|| format!("Failed to read PDF from: {}", file.to_str().unwrap()))?;
 
-            info!("Iterating through pages...");
             for (page_number, page) in (1u32..).zip(document.page_iter()) {
                 if !pages.contains_key(&page_number) {
                     warn!(
@@ -223,6 +238,10 @@ impl Execute for Merge {
                         return !self.exclude.iter().any(|e| subtype == e);
                     })
                     .for_each(|annotation| {
+                        trace!(
+                            "Found annotation on page {page_number} in document \
+                             #{document_number}, inserting it inside reference document"
+                        );
                         let id = main.add_object(annotation.clone());
                         annotations_map
                             .entry(page_number)
@@ -232,9 +251,14 @@ impl Execute for Merge {
             }
         }
 
+        info!("Updating the annotation arrays in reference document");
         for (page_number, new_ann) in annotations_map.iter_mut() {
             match pages.get(page_number) {
                 Some(page_id) => {
+                    debug!(
+                        "Retrieving a mutable reference to page's {page_number} annotations in \
+                         reference document"
+                    );
                     let current_ann = get_page_annotations_mut(&mut main, *page_id);
 
                     current_ann.append(new_ann);
@@ -256,7 +280,7 @@ impl Execute for Merge {
     }
 }
 
-/// Get annotation ids to a given page id.
+/// Get annotation ids of a given page id.
 fn get_page_annotations(document: &Document, page_id: ObjectId) -> Vec<ObjectId> {
     let page = document.get_dictionary(page_id).unwrap();
     let mut ids = vec![];
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use anyhow::Result;
 use clap::Parser;
+use log::error;
 
 mod cli;
 
 use cli::Cli;
 
-fn main() -> Result<()> {
+fn main() {
     let cli = Cli::parse_from(wild::args());
 
     pretty_env_logger::formatted_builder()
         .filter_level(cli.verbose.log_level_filter())
         .init();
 
-    cli.execute()
+    match cli.execute() {
+        Err(e) => error!("{e:#}"),
+        _ => (),
+    };
 }
